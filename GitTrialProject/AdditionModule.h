0aedd9f1e04b4941cd7a5b0fe83207ce653e75f5
hprashant <prsnt@iastate.edu>
2014-07-08 03:35:16 -0500

    Addition module changed

--- GitTrialProject/AdditionModule.h
+++ GitTrialProject/AdditionModule.h
@@ -1,1 +1,20 @@
 // This class is meant to calculate addition between n numbers.
+#ifndef  ADDITIONMODULE_H
+#define ADDITIONMODULE_H
+
+class Add{
+
+public:
+	Add();
+	~Add();
+
+private: // attribute
+	int a;
+	int b;
+
+public: //behaviour
+	int getAddResult()const;
+	int setValues(int tempa, int tempb);
+};
+
+#endif
