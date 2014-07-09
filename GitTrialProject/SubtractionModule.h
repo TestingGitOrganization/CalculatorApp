c2e5fffd7dff5ead02835edc1b90beff1a8ec5e4
hprashant <prsnt@iastate.edu>
2014-07-09 01:20:13 -0500

    Subtraction module design implemented
    The task needs to be done by Ash

--- GitTrialProject/SubtractionModule.h
+++ GitTrialProject/SubtractionModule.h
@@ -3,3 +3,17 @@
 *  class  : does subtraction between 2 numbers
 */
 /************************************************************************/
+
+class SubtractionModule{
+
+public:
+	SubtractionModule();
+	~SubtractionModule();
+
+public: //attribute
+	int a;
+	int b;
+
+private: // attribute
+	int result;
+};
